407b46ed3a51e192866d3d17d8ef2fa390a98578
Meseretbekele <bekelemeseret364@gmail.com>
2022-04-07 08:11:14 -0700

    my #6 c task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 after program execution
+ */
+int main(void)
+{	
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+printf("size of a char:%d byte(s)\n", (unsigned long)sizeof(d));
+printf("size of an int:%d byte(s)\n", (unsigned long)sizeof(a));
+printf("size of a long int: byte(s))\n", (unsigned long) sizeof(b));
+printf("size of a long long int: byte(s)\n", (unsigned long) sizeof(c));
+printf("size of a float: byte(s)\n", (unsigned long) sizeof(f));
+return (0);
+}
