e27525eaad4fa71c6b83d973816647b2c5a36f9d
Meseretbekele <bekelemeseret364@gmail.com>
2022-04-08 02:24:22 -0700

    my #6 0x01.c task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return:0 always success
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
