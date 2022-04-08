e7639876837cb2c13245d1bf5afeb47f49e0180b
Meseretbekele <bekelemeseret364@gmail.com>
2022-04-08 01:13:13 -0700

    my #8 0x00.c task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,18 @@
 #include <unistd.h>
 
 /**
- * main - entry point
  *
- * Return: 0 after programm execution
+ * main - Entry point
+ *
+ *
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *
+ * Return: Always 0 (Success)
+ *
  */
+
 int main(void)
 {
-write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
