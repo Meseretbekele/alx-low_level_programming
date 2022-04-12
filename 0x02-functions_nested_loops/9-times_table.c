777538b8cef9ff458ce348bffaf6b678452e38df
Meseretbekele <bekelemeseret364@gmail.com>
2022-04-12 05:03:46 -0700

    0x02.task #9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,7 @@
 /**
  * times_table - that prints the 9 times table
  *
- * Return: no return 
+ * Return: no return
  */
 void times_table(void)
 {
@@ -11,11 +11,13 @@ for (a = 0; a <= 9; a++)
 {
 _putchar(48);
 for (b = 1; b <= 9; b++)
-{																	c = a * b;
+{
+c = a * b;
 _putchar(44);
 _putchar(32);
-if (c <= 9)					
-{																	_putchar(32);
+if (c <= 9)
+{
+_putchar(32);
 _putchar(c + 48);
 }
 else
@@ -23,7 +25,7 @@ else
 _putchar((c / 10) + 48);
 _putchar((c % 10) + 48);
 }
-_putchar('\n');
 }
+_putchar('\n');
 }
 }
