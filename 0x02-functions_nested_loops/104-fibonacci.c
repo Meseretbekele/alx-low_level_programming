affe32e39041231d8b0e4d29eabb77de821c6ca9
Meseretbekele <bekelemeseret364@gmail.com>
2022-04-13 00:20:48 -0700

    0x02.c task #16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -25,24 +25,17 @@ a_head = a / 1000000000; /* break larger num into 2 parts */
 a_tail = a % 1000000000;
 b_head = b / 1000000000;
 b_tail = b % 1000000000;
-
 for (; counter < 99; counter++)
 {
-
-overflow = (a_tail + b_tail) / 1000000000;
+overflow = (a_tail + b_tail) / (1000000000);
 sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
 sum_head = (a_head + b_head) + overflow;
-
 printf(", %lu%lu", sum_head, sum_tail);
-
 a_head = b_head;
 a_tail = b_tail;
 b_head = sum_head;
 b_tail = sum_tail;
-
 }
-
 printf("\n");
-
 return (0);
 }
