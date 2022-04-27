93a6b44f14744a75d1614224ebac12321f9fdbdd
Meseretbekele <bekelemeseret364@gmail.com>
2022-04-27 12:33:25 -0700

    recursion task

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * int _pow_recursion(int x, int y)- returns the value of x raised to the power of y.
+ * int _pow_recursion(int x, int y)- returns the value of x 
+ * raised to the power of y.
  * @x: given number
  * @y: exponent
  * Return: value of x raised to the power of y
